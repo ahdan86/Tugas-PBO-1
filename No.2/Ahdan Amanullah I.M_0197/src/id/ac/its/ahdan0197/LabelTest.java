0c003973cadf280fa32d6fa33076bfbd9496164a
ahdan86 <ahdanamanullah@gmail.com>
2020-12-02 16:18:57 +0700

    Adding Project2_0197

---
+++ No.2/Ahdan Amanullah I.M_0197/src/id/ac/its/ahdan0197/LabelTest.java
@@ -0,0 +1,14 @@
+package id.ac.its.ahdan0197;
+
+import javax.swing.JFrame;
+
+public class LabelTest {
+
+	public static void main(String[] args) {
+		LabelFrame labelFrame = new LabelFrame();
+		labelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		labelFrame.setSize(720,720);
+		labelFrame.setVisible(true);
+	}
+
+}
