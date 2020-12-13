9ecedd7543f8138426f53764d567c521ed95a78d
axel briano <axelbrians@gmail.com>
2020-12-13 23:14:10 +0700

    Axel / Added whole code for collition detection

---
+++ Collision Detection/Collision Detection/src/id/ac/its/kelompok/SpaceShip.java
@@ -0,0 +1,101 @@
+package id.ac.its.kelompok;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ImageIcon;
+
+public class SpaceShip extends Sprite {
+
+    private int dx;
+    private int dy;
+    private List<Missile> missiles;
+
+    public SpaceShip(int x, int y) {
+        super(x, y);
+
+        initSpaceShip();
+    }
+
+    private void initSpaceShip() {
+
+        missiles = new ArrayList<>();
+
+        loadImage("src/resources/craft.png");
+        getImageDimension();
+    }
+
+    public void move() {
+
+        if(y + dy + height > 300){
+            y = y;
+        }else if (y + dy < 0){
+            y = y;
+        }else {
+            y += dy;
+        }
+
+        if(x + dx + width > 400){
+            x = x;
+        }else if (x + dx < 0){
+            x = x;
+        }else {
+            x += dx;
+        }
+    }
+
+    public List<Missile> getMissiles() {
+        return missiles;
+    }
+
+    public void fire() {
+        missiles.add(new Missile(x + width, y + height / 2));
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_SPACE) {
+            fire();
+        }
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -2;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 2;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -2;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 2;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+}
