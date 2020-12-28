9351bb7b7b60a817bee5f3447cb2c886d0c4eafc
ahdan86 <ahdanamanullah@gmail.com>
2020-12-28 23:01:33 +0700

    Ahdan / Push Reference for Final Project

---
+++ Final Project/ReferensiFinalProject/src/id/ac/its/kelompok/WindowGame.java
@@ -0,0 +1,46 @@
+package id.ac.its.kelompok;
+
+import java.awt.Component;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public class WindowGame {
+	public static final int WIDTH = 445, HEIGHT = 629;
+
+    private Board board;
+    private Component title;
+    private JFrame window;
+
+    public WindowGame() {
+
+        window = new JFrame("Tetris");
+        window.setSize(WIDTH, HEIGHT);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+
+        board = new Board();
+        title = new Title(this);
+
+        window.addKeyListener(board);
+        window.addKeyListener((KeyListener) title);
+
+        window.add(title);
+
+        window.setVisible(true);
+    }
+
+    public void startTetris() {
+        window.remove(title);
+        window.addMouseMotionListener(board);
+        window.addMouseListener(board);
+        window.add(board);
+        board.startGame();
+        window.revalidate();
+    }
+
+    public static void main(String[] args) {
+        new WindowGame();
+    }
+}
